4c80b3d56405b63d4eeb4a11dc6a6cba0b58c137
Federico-Gaiani <s309900@studenti.polito.it>
2025-03-29 22:18:24 +0100

    concept, prove varie, commenti

--- num_complessi/complex.hpp
+++ num_complessi/complex.hpp
@@ -1,52 +1,62 @@
 #pragma once
 
-
+/*
+Da cppreference.com : 
+"The concept floating_point<T> is satisfied if and only if T is a floating-point type."
+I tipi floating point standard sono: float, double e long double.
+In questo caso usiamo il concept, che è un predicato e viene valutato a tempo di compilazione, con 
+il vincolo (constraint) requires.
+*/
 
 template<typename F> requires std::floating_point<F>
-class complex_number
-{
+class complex_number{
     F Re;
 	F Im;
-public:
+	
+	public:
+	
+	// Costruttore di default
     complex_number()
         : Re(0), Im(0)
     {}
     
-
+	// Costruttore user-defined (un parametro, che assegno alla perte reale)
+	// Uso di "explicit", non accetto che si possibile scrivere nel main:
+	// complex_number c1 = 3;
     explicit complex_number(F r)
         : Re(r), Im(0)
     {
         std::cout << "converting constructor called" << std::endl;
     }
     
-
+	// Costruttore user-defined (due parametri, che assegno alla perte reale e immaginaria rispettivamente)
     complex_number(F r, F i)
         : Re(r), Im(i)
     {}
     
 /*
-il const serve a esplicitare il fatto che non modifico l'istanza sulla quale lo chiamo
+	Metodi che ritornano parte reale e immaginaria rispettivamente. Non modificano l'istanza sulla quale 
+	sono chiamati (const)
 */
     F num_Re(void) const {
         return Re;
     }
-    
 
     F num_Im(void) const {
         return Im;
     }
 	
-	
+	// Overload operatore += tra tipi complex_number
 	complex_number& operator+=(const complex_number& other) {
-		Re += other.Re; // con attributo
+		Re += other.Re; 
         Im += other.Im;
 		
-        //Re += other.num_Re(); // con metodo
+        //Re += other.num_Re(); // soluzione alternativa con metodo
         //Im += other.num_Im();
         return *this;
 	}
-
-
+	
+	// Overload operatore + tra tipi complex_number
 	complex_number operator+(const complex_number& other) const {
         complex_number ret = *this;
         ret += other;
@@ -54,23 +64,26 @@ il const serve a esplicitare il fatto che non modifico l'istanza sulla quale lo 
     }
 	
 	/*
-	OSS: nell'overload a volte uso l'& e a volte no. Perché?
-	Il motivo è che con il + la funzione vuole ritornare un nuovo complex_number,
-	invece con il += vogliamo modificare il valore dell'istanza a sinistra dell'operatore, usiamo dunque un rif.	
+	OSS: nell'overload a volte nel tipo di ritorno uso l'& e a volte no. Perché?
+	Il motivo è che nel caso, ad es., del + la funzione vuole ritornare un nuovo complex_number,
+	invece con il += vogliamo modificare il valore dell'istanza a sinistra dell'operatore (quando viene 
+	chiamato), usiamo dunque un rif.	
 	*/
 	
-	
+	// Overload operatore: tipo complex_number += tipo F
 	complex_number& operator+=(const F& other) {
         Re += other;
         return *this;
 	}
-	
+		
+	// Overload operatore: tipo complex_number + tipo F
 	complex_number operator+(const F& other) const {
         complex_number ret = *this;
         ret += other;
         return ret;
     }
-	// moltiplicazione
+		
+	// Overload operatore: *= tra tipi complex_number 
 	complex_number& operator*=(const complex_number& other) {
 		F a = Re;
 		F b = Im;
@@ -79,19 +92,21 @@ il const serve a esplicitare il fatto che non modifico l'istanza sulla quale lo 
         return *this;
 	}
 	
+	// Overload operatore: * tra tipi complex_number
 	complex_number operator*(const complex_number& other) const {
         complex_number ret = *this;
         ret *= other;
         return ret;
     }
 	
-	
+	// Overload operatore: tipo complex_number *= tipo F
 	complex_number& operator*=(const F& other) {
         Re *= other;
 		Im *= other;
         return *this;
 	}
 	
+	// Overload operatore: tipo complex_number * tipo F
 	complex_number operator*(const F& other) const {
         complex_number ret = *this;
         ret *= other;
@@ -100,29 +115,32 @@ il const serve a esplicitare il fatto che non modifico l'istanza sulla quale lo 
 	
 };
 
-template<typename F> //requires std::floating_point<F>
+// Nel caso dell'operazione F + complex_number, garantisco la commutatività commutando manualmente gli 
+// addendi e usando le operazioni gia implementate. Analogamente per il *.
+template<typename F> requires std::floating_point<F>
 complex_number<F> operator+(const F& d, const complex_number<F>& com){
     return com + d;
 }
 
 
 //commutatività moltiplicazione
-template<typename F> //requires std::floating_point<F>
+template<typename F> requires std::floating_point<F>
 complex_number<F> operator*(const F& d, const complex_number<F>& com){
     return com * d;
 }
 
 
 /*
-A questa funzione passo per riferimento costante ...
+A questa funzione passo per riferimento costante il numero complesso di cui voglio ottenere il complesso
+coniugato. Non modifica il valore della variabile a cui la funzione è applicata.
 */
-template<typename F> //requires std::floating_point<F>
-complex_number<F> conjugate(const complex_number<F>& r) {
-    return complex_number<F>(r.num_Re(), -r.num_Im());
+template<typename F> requires std::floating_point<F>
+complex_number<F> conjugate(const complex_number<F>& c) {
+    return complex_number<F>(c.num_Re(), -c.num_Im());
 }
 
-
-template<typename F> //requires std::floating_point<F>
+// Overload operatore << per stampare i complex_number
+template<typename F> requires std::floating_point<F>
 std::ostream& operator<<(std::ostream& os, const complex_number<F>& r) {
     if (r.num_Im() != 0){
 		if (r.num_Im()<0){
--- num_complessi/main.cpp
+++ num_complessi/main.cpp
@@ -13,22 +13,22 @@ int main(void) {
 	cmp r3(4.5);
 	// cmp r4 = 5 ; // da errore (come voluto) dovuto all'uso di explicit
 	
-	std::cout << r1<<std::endl;
-	std::cout << conjugate(r1)<<std::endl;
-	std::cout << r1.num_Im()<<std::endl;
+	std::cout <<"Stampo 1.5 - 3.3i: "<< r1<<std::endl;
+	std::cout << "Stampo il complesso coniugato: "<<conjugate(r1)<<std::endl;
+	// std::cout << "Stampo la patre immaginaria: "<<r1.num_Im()<<std::endl;
 	
-	std::cout << r2<<std::endl;
-	std::cout << r1<<std::endl;
 	r2+=r1;
-	std::cout << r2<<std::endl;
-	std::cout << r2+r1<<std::endl;
+	// std::cout <<"Prova overload +=: "<< r2<<std::endl;
+	// std::cout <<"Prova overload +: "<< r2+r1<<std::endl;
 	
 	r2+=8.8;
-	std::cout << r2 <<std::endl;
+	// std::cout <<"Prova overload += con floating point: "<< r2 <<std::endl;
 	double f = 4.5;
 	cmp r5 = f+r1;
-	std::cout << r5 <<std::endl;
+	// std::cout <<"Prova commutatività: "<< r5 <<std::endl;
+	
 	/*
+	// Prove moltiplicazione
 	cmp r6(2,2);
 	cmp r7(3,-4);
 	std::cout << r6*r7 <<std::endl;
