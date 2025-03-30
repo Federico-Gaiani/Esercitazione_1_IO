accdcf1843879227227ca042bfabce5f03e10d99
Federico-Gaiani <s309900@studenti.polito.it>
2025-03-30 10:05:04 +0200

    modifica stampa, commenti

--- Exercise2/main.cpp
+++ Exercise2/main.cpp
@@ -1,8 +1,8 @@
 # include <fstream>
 # include <iostream>
-# include <cmath>
 # include <iomanip>
 
+// funzione che implementa la biiezione da [1,5] a [-1,2]
 double cambio_var(double x){
 	double y=0.5+0.75*(x-3);
 	return y;
@@ -10,35 +10,41 @@ double cambio_var(double x){
 
 int main()
 {
+	// Apro il file input
+	std::ifstream file("data.txt"); 
 	
-	std::ifstream file("data.txt"); // il file deve essere nella cartella debug
 	if (file.fail()){
-		std::cerr<<"errore nell'apertura del file"<<std::endl;
+		std::cerr<<"Errore nell'apertura del file"<<std::endl;
 		return 1;
 	}
-	
+	// File di output
 	std::string filename = "result.txt";
 	std::ofstream ofs(filename);
+	
 	if (! ofs.is_open()){
-		std::cerr<<"errore apertura file output"<<std::endl;
+		std::cerr<<"Errore apertura file output"<<std::endl;
 		return 1;
 	}
 	
+	// Intestazione result.txt
+	ofs<<"# N Mean"<<std::endl;
 	
+	// Ciclo con cambio variabile, calcolo media, stampa su output
 	double val;
 	double sum=0;
+	double media =0;
 	int k=1;
-	ofs<<"# N Mean"<<std::endl;
+	
 	while(file>>val){
 		double new_val= cambio_var(val);
-		
 		sum+=new_val;
-		double media=sum/k;
-		//std::cout<<std::setprecision(16)<<std::scientific<<media<<std::endl;
+		media=sum/k;
+		ofs<<k<<" "<<std::setprecision(16)<<std::scientific<<media<<std::endl;
 		k++;
-		ofs<<std::setprecision(16)<<std::scientific<<media<<std::endl;
-		
 	}
+	
+	// Chiusura file
+	file.close();
 	ofs.close();
 	
     return 0;
